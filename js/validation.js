3687ad8c4f0a0585f0697e1f94b8ce514ecf95aa
JanviLakani <lakanijanvi1999@gmail.com>
2025-04-02 10:06:51 +0530

    add

--- js/validation.js
+++ js/validation.js
@@ -1,53 +1,38 @@
-
-
 function handlesubmit() {
-    
-    event.preventDefault();
-    
-    // console.log("sjkdhsfj");
+  event.preventDefault();
 
-    let name=document.getElementById("fname").value;
-    let fage = document.getElementById('age').value;
+  // console.log("sjkdhsfj");
 
-    console.log('name');
-    console.log(fage);
+  let name = document.getElementById("fname").value;
+  let fage = document.getElementById("age").value;
 
-    if (name === '') {
-        document.getElementById('spanfname').innerHTML = "please enter youfname";
-    } else {
-        var regex = /^[a-zA-Z ]{2,30}$/;
+  console.log("name");
+  console.log(fage);
 
-       if ( regex.test(name)) {
-        document.getElementById('spanfname').innerHTML = "please enter valid name";
-       }
-
-        document.getElementById('spanfname').innerHTML = "";
+  if (name === "") {
+    document.getElementById("spanfname").innerHTML = "please enter youfname";
+  } else {
+    var regex = /^[a-zA-Z ]{2,30}$/;
+    if (regex.test(name)) {
+      document.getElementById("spanfname").innerHTML = "";
     }
 
+    document.getElementById("spanfname").innerHTML = "please enter valid name";
+  }
 
-    if (fage !== '') {
-        if (isNaN(fage)) {
-             document.getElementById('spanage').innerHTML="please enter age in number"
-        } else if (fage >= 0) {
-            document.getElementById('spanage').innerHTML=""
-        } else {
-             document.getElementById('spanage').innerHTML="please enter valid age "
-        }
+  if (fage !== "") {
+    if (isNaN(fage)) {
+      document.getElementById("spanage").innerHTML =
+        "please enter age in number";
+    } else if (fage >= 0) {
+      document.getElementById("spanage").innerHTML = "";
     } else {
-        document.getElementById('spanage').innerHTML="please enter your age "
+      document.getElementById("spanage").innerHTML = "please enter valid age ";
     }
+  } else {
+    document.getElementById("spanage").innerHTML = "please enter your age ";
+  }
 
-    // document.getElementById('spanfname').innerHTML = "jhjhjhjhjh";
-    // document.getElementById('spanage').innerHTML;
-
-    
-
-    
-
-
-    
-
-
-    
-    
+  // document.getElementById('spanfname').innerHTML = "jhjhjhjhjh";
+  // document.getElementById('spanage').innerHTML;
 }
