be6a952051c7126c5c10b0d0e0262ede85f455c4
JanviLakani <lakanijanvi1999@gmail.com>
2025-04-04 10:20:37 +0530

    add

--- js/treatment1.js
+++ js/treatment1.js
@@ -1,44 +1,42 @@
 function handletretment() {
+  event.preventDefault();
 
-    let clean =document.getElementById("tre").value
-    console.log(clean);
+  let clean = document.getElementById("tre").value;
+  // console.log(clean);
 
-    let tretment1,seating,costing;
+  let tretment1, seating, costing;
 
-    // console.log(tretment1);
-    
+  // console.log(tretment1);
 
-    if (clean === 'tc') {
-        tretment1 = "teethcleaning";
-        seating = 1;
-        costing = 500;
+  if (clean === "tc") {
+    tretment1 = "teethcleaning";
+    seating = 1;
+    costing = 500;
+  } else if (clean === "fl") {
+    tretment1 = "fillings";
+    seating = 2;
+    costing = 1500;
+  } else if (clean === "rct") {
+    tretment1 = "root canal tretment";
+    seating = 4;
+    costing = 2500;
+  } else if (clean === "rcc") {
+    tretment1 = "ret+cover";
+    seating = 6;
+    costing = 12000;
+  } else if (clean === "bi") {
+    tretment1 = "braces/invisalign";
+    seating = 12;
+    costing = 35000;
+  }
 
-    } else if ( clean === 'fl')  {
-        tretment1 = "fillings";
-        seating = 2;
-        costing = 1500;
-    } else if (clean === "rct") {
-        tretment1 = "root canal tretment";
-        seating = 4;
-        costing = 2500;
-    } else if (clean === "rcc") {
-        tretment1 = "ret+cover";
-        seating = 6;
-        costing = 12000;
-    } else if (clean === "bi") {
-        tretment1 = "braces/invisalign";
-        seating = 12;
-        costing = 35000;
-    }
+  console.log(tretment1, seating);
+  console.log(costing);
 
-    console.log(tretment1,seating);
-    console.log(costing);
-    
-  
-    
+  let print = ``;
 
-    
-let print = `
+  if (clean !== "st") {
+    print = `
     
 <table border>
    <tr>
@@ -47,23 +45,35 @@ let print = `
        <th>costing</th>
    </tr> 
 
-`; 
-print += '<tr>'
-print += `<td>${tretment1}</td>`
-print += `<td>${seating}</td>`
-print += `<td>${costing}</td>`
-print +=  `</tr>`
+`;
+    print += `<tr>`;
+    print += `<td>${tretment1}</td>`;
+    print += `<td>${seating}</td>`;
+    print += `<td>${costing}</td>`;
+    print += `</tr>`;
+  }
 
 
-// `;
+
+//   console.log(print);
+
+  document.getElementById("add").innerHTML = print;
 
 
-console.log(print);
 
-document.getElementById("add").innerHTML = print;
-    
-    
-    
+// ***************************************************** 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
-
-
