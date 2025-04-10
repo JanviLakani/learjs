f8321b4b1f388e15e89e07e9c832535973a8586b
JanviLakani <lakanijanvi1999@gmail.com>
2025-04-11 01:36:38 +0530

    ADD

--- js/3_array_intro.js
+++ js/3_array_intro.js
@@ -71,12 +71,12 @@
 
 // let arr =[10,20,30,40,50,60]    /// both mate chhe delete and add mate
 
-let arr = [1,2,3,4,5,6]
+// let arr = [1,2,3,4,5,6]
 // delete
 // const ans = arr.pop()                 //delete at last   // return removed element
 // const ans = arr.shift()               //delete at start   //return removed element
 // const ans = arr.splice(3,2)              //delete at specific index, return array of removed elements
-const ans = arr.splice(2,3)  
+// const ans = arr.splice(2,3)  
 
 // add
 // const ans= arr.push(999);              // add at last    // return lenth of array
@@ -84,7 +84,7 @@ const ans = arr.splice(2,3)
 // const ans = arr.splice(3,0,31,32,33)  // add at specific index  // return empty array
                                         
 
-console.log(arr ,ans);               /// both mate chhe delete and add mate
+// console.log(arr ,ans);               /// both mate chhe delete and add mate
 
 
 
@@ -104,12 +104,123 @@ console.log(arr ,ans);               /// both mate chhe delete and add mate
 
 //////////////////////////////////////////////////////////////////////
 
+/// H.W  11-04-2025 ///
+
+///////////////////////////////////////////////////////////
+
+// 1 slice()  
+
+// let arr= ["janvi" ,"nijal","bansi" ,"pinal" ,"radhi"]
+// const ans = arr.slice(2 ,4);     //return :- je value tme removed karavi chhe te bija variable ma add thai jashe 
+// const ans = arr.slice(-3);
+
+// console.log(arr ,ans);
+
+
+///////////////////////////////////////////////////////////////
+
+
+// map()
+
+// let arr =[ 5,10,15,20,25,30,35]
+// let ans=arr.map(test);
+// function test(x){
+//     return x+10;
+// }
+
+// console.log(ans);
+
+
+////////////////////////////////////////////////////////////////
+
+
+// filter()
+
+// let ages = [10 ,2,30,40,50]
+// const ans =ages.filter((v) => v>18
+// console.log(ans);     // ans filter ni condition ma je pass thashe te badhi value ne new add ma add karavi deshe mean return karshe
+
+
+////////////////////////////////////////////////////////////////
+
+
+// reduce()
+
+// let add=[1,3,4,5,6];
+// let adds=add.reduce((j1,j2) => {
+//     return j1+j2
+// })
+
+// console.log(adds);
+
+
+///////////////////////////////////////////////////////////////
+
+
+//indexOf()
+
+// let fruit = ["mango" , "chiku" , "apple" , "chiku" , "peru"] 
+// const ans =fruit.indexOf("chiku",2);
+// console.log(ans);             // return first occurence index
+
+
+///////////////////////////////////////////////////////////////////
+
+//lastindexOf()
+
+// let cloth = [ "dres" , "kurta" ,"plaza", "anarkali" , "plaza" ,"shrara" ,"top" ];
+// const ans =cloth.lastIndexOf("plaza")
+// console.log(ans);
+
+
+//////////////////////////////////////////////////////////////////
+
+// every()
+
+// let sub = [80,90,89,85,99,89]
+// const ans =sub.every((v) => v >=80);
+// console.log(ans);
+
+
+///////////////////////////////////////////////////////////////////
+
+// findindex()
+
+// let arr = [5,10,15,20,25,30,35,40,55,60]
+// const ans = arr.findIndex((v) => v>50)  //ans return first occurence of elementindex when match
+// console.log(ans);
 
 
 
+////////////////////////////////////////////////////////////////
+
+
+// fill()
+
+// let fruit= ["mango" , "peru" ,"apple "];
+// fruit.fill("banana");
+// console.log(fruit);
+
+////////////////////////////////////////////////////////////////
+
+// sort()
+
+// let nam = [ "zara" , "viya" , "siya" , "kavya" ,"janvi" , "bansi", "sanvi"]
+// const ans =nam.sort()
+// console.log(ans);     // a to z pramane reurn karshe
+
+
+////////////////////////////////////////////////////////////////
+
+// reverse()
+
+// let nam = [ "zara" , "viya" , "siya" , "kavya" ,"janvi" , "bansi", "sanvi"]
+// const ans =nam.reverse()
+// console.log(ans);       // je valulu hal ma teni all reverse value return karshe 
 
 
 
+////////////////////////////////////////////////////////////////////////////
 
 
 
