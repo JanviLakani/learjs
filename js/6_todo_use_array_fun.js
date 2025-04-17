110df34689b62e6668d0b9ec125050213d0b30d5
JanviLakani <lakanijanvi1999@gmail.com>
2025-04-17 13:31:30 +0530

    add

--- js/6_todo_use_array_fun.js
+++ js/6_todo_use_array_fun.js
@@ -1,33 +1,97 @@
+let newtodo = [];
 
-let newtodo =[];
-function handleonsubmit() {
+let update =null;
 
+function handleonclick(i){
 
-    event.preventDefault(); 
-        
-let todoname=document.getElementById("name").value;
+    console.log(i);
+    
+    newtodo.splice(i,1);
 
-console.log(todoname);
+    
+    
+    display();
+}
 
+function handleedit(i) {
 
+    
 
-if (todoname === "") {
-    document.getElementById("nameerror").innerHTML="please enter your todo";
-} else {
+    console.log(i);
+    
+    console.log(i,newtodo[i])
 
-    let todolist =/^[a-zA-Z ]{2,30}$/;
-
-    if (todolist.test(todoname)) {
-        document.getElementById("nameerror").innerHTML="";
-    } else {
-        document.getElementById("nameerror").innerHTML="please enter valid todo";
-    }
+    document.getElementById("name").value=newtodo[i];
+    
 }
 
 
-let ans=newtodo.push(todoname);
+function display(){
+        
 
-console.log(ans);
+
+    let print = ``;
+
+    newtodo.map((v, i) => {
+      print += `<li>${v}<button onclick="handleonclick(${i})">d</button>
+                        <button onclick="handleedit(${i})">xx</button>
+      </li>`;
+    });
+
+
+
+    document.getElementById("disk").innerHTML = print;
+
+    
+
+
+}
+
+
+
+function handleonsubmit() {
+  event.preventDefault();
+
+  let todoname = document.getElementById("name").value;
+
+  console.log(todoname);
+
+  if (todoname === "") {
+    document.getElementById("nameerror").innerHTML = "please enter your todo";
+  } else {
+    let todolist = /^[a-zA-Z ]{2,30}$/;
+
+    if (todolist.test(todoname)) {
+      document.getElementById("nameerror").innerHTML = "";
+
+
+    //   newtodo.push(todoname);
+
+    //   console.log(newtodo);
+
+    if (update === null) {
+        newtodo.push(todoname);
+    } else {
+        newtodo(update);
+        update=null
+    }
+
+
+ 
+    display();
+
+
+    } else {
+      document.getElementById("nameerror").innerHTML =
+        "please enter valid todo";
+    }
+
+
+  }
+
+
+
+       
 
 
 }
