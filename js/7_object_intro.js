3419a056aaf2fd3f1e57c6002d94aa0c0b6ac4be
JanviLakani <lakanijanvi1999@gmail.com>
2025-04-18 12:34:07 +0530

    add

--- js/7_object_intro.js
+++ js/7_object_intro.js
@@ -82,5 +82,156 @@
 // console.log(fees);
 
 
-//  *********************************************************** update 
+//  *********************************************************** 
+
+//  class 
+
+// let arr = [10,20,30];  // array
+
+// let obj = {
+//     id : 10,
+//     age : 20,
+//     per : 30,
+//     city : "palitana"
+// }
+
+// console.log(obj);
+
+
+
+// get 
+
+// for ( let k in obj) {        // all key get karva mate for in k use karvu
+//     console.log(k, obj[k]);
+    
+// }
+
+
+// add
+
+// obj.city = "surat";
+
+
+
+// obj["city2"]="vapi";
+
+// console.log(city2);
+
+
+// get 
+
+// console.log(obj.age, obj["age"]);
+
+
+
+// // update 
+
+// obj.age=99;
+
+// delete obj.per;
+
+// console.log(obj);
+
+// ******************************* 1
+
+// object assignment 
+
+
+// const person = {
+//     name: "amit",
+//     Age : "20"
+// }
+
+// console.log(person.name, person.Age);
+
+// ****** 2
+
+// const person = [
+//     {
+//         name: "Amit",
+//         age: 25,
+//     },
+//     {
+//         name: "Piyush",
+//         age: 40,
+//     }
+// ];
+
+// person.map((v,i) =>{
+//     console.log(v,v.name, v.age);
+//     // console.log(v);
+    
+//     // console.log(v, person.v);
+    
+    
+// })
+
+
+
+// ********** 3
+
+// const person = [    
+//     {
+//         name: "Amit",
+//        age: 25,
+
+//        course: [
+//            "c",
+//            "html"
+//        ]
+//     },
+    
+//     {
+//         name: "Ami",
+//        age: 22,
+//        course: [
+//            "c++",
+//            "java"
+//        ]
+//     }
+// ]  
+
+
+// person.map((v,i) => {
+//     console.log(v.name, v.age,);
+//     v.course.map((v1,i1) => {
+//         console.log(v1);
+        
+//     })
+// })
+
+// ************************************ 4 
+
+
+// const myObj = {
+//     name:"jd",
+//     age:20,
+//     cars:{
+//         car1:"ford",
+//         car2:"breza",
+//         car3:"BMW"
+//     }
+// }
+
+// for (let k in myObj) {
+
+//     // console.log(k, myObj[k]);
+    
+//     if (cars === myObj) {
+//         console.log(j, myObj[j]);
+        
+//     } else {
+//         console.log(k, myObj[k]);
+//     }
+
+
+// }
+
+// console.log(myObj);
+
+
+
+
+
+
 
