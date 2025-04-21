d4dcfbfa052b96cc9105845de1d198e4b2d55d08
JanviLakani <lakanijanvi1999@gmail.com>
2025-04-21 12:30:57 +0530

    add

--- js/9_object_method.js
+++ js/9_object_method.js
@@ -70,7 +70,110 @@
 // delete student.name;
                                         //Freeze hone ke baad object immutable ban jata hai.
 
-// *********************************************************** 
+// ***********************************************************  // v.seet[0]
+
+// class
+
+// let person = {
+//     name : "pinal",
+//     age : "22"
+// }
+
+// console.log(Object.keys(person));  // return array of keys
+
+// console.log(Object.values(person)); // return array of value
+
+// console.log(Object.entries(person)); // return array of property
+
+// console.log(Object.fromEntries(Object.entries(person))); // return ofject from array of properties array
+
+// console.log(person.hasOwnProperty("city")); 
+
+// Object.freeze(person);  // freeze object
+
+// person.city="vapi";
+
+// console.log(person);
+
+
+// ******************************************************************** 
+
+let institues = [
+    {
+        name: 'ABC IT Institute',
+        seat: [
+            {
+                react: 15,
+                node: 20,
+                full_stack: 10,
+                ui_ux: 0
+            },
+        ]
+    },
+    {
+        name: 'XYZ IT Institute',
+        seat: [
+            {
+                react: 0,
+                node: 70,
+                full_stack: 0,
+                ui_ux: 10
+            },
+        ]
+    },
+    {
+        name: 'PQR IT Institute',
+        seat: [
+            {
+                react: 7,
+                node: 0,
+                full_stack: 0,
+                ui_ux: 0
+            },
+        ]
+    },
+    {
+        name: 'MNP IT Institute',
+        seat: [
+            {
+                react: 0,
+                node: 0,
+                full_stack: 0,
+                ui_ux: 0
+            },
+        ]
+    }
+]
+
+
+let vdate=institues.map((v,i)=> {
+    return {
+        name : v.name,
+        seat : [Object.fromEntries(Object.entries(v.seat[0]).filter(([k , v]) => v > 0 ))]
+        
+    }
+
+})
+
+
+console.log(vdate);   
+
+                                        // key hase e return karshe 0 > 
+
+let vvdata=vdate.filter((v)=> Object.keys(v.seat[0]).length > 0 )
+
+console.log(vvdata);
+
+
+
+
+
+
+
+
+
+
+
   
   
 
